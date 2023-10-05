64aa8e18de7187a1c0e276185dc6135d9ffcda7d
As1ss <alexis.lopbri@educa.jcyl.es>
2023-10-06 01:02:51 +0200

    Updated some stuff

--- Practica2/app/src/main/java/al/if05/practica2/MainActivity.java
+++ Practica2/app/src/main/java/al/if05/practica2/MainActivity.java
@@ -3,6 +3,10 @@ package al.if05.practica2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] cities = getResources().getStringArray(R.array.cityList);
+        String[] hobbies = getResources().getStringArray(R.array.hobbiesList);
+
+        Spinner spCity = findViewById(R.id.spinnerCity);
+        ArrayAdapter<String> adapterArrayCities = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, cities);
+        adapterArrayCities.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spCity.setAdapter(adapterArrayCities);
+
+        Spinner spHobbies = findViewById(R.id.spinerInterest);
+        ArrayAdapter<String> adapterArrayHobbies = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, hobbies);
+        spHobbies.setPrompt("Hobbies");
+        spHobbies.setAdapter(adapterArrayHobbies);
+
+
     }
 }
