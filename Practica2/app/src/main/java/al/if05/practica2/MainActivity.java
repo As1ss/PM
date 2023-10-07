81a58ac7ff037bd454a8f344095ef93ee1436295
As1ss <alexis.lopbri@educa.jcyl.es>
2023-10-07 21:31:54 +0200

    Practica 2 completed.

--- Practica2/app/src/main/java/al/if05/practica2/MainActivity.java
+++ Practica2/app/src/main/java/al/if05/practica2/MainActivity.java
@@ -17,20 +17,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String[] cities = getResources().getStringArray(R.array.cityList);
-        String[] hobbies = getResources().getStringArray(R.array.hobbiesList);
+
 
         Spinner spCity = findViewById(R.id.spinnerCity);
         ArrayAdapter<String> adapterArrayCities = new ArrayAdapter<>(this, R.layout.spinner_item, cities);
         adapterArrayCities.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spCity.setAdapter(adapterArrayCities);
 
-        Spinner spHobbies = findViewById(R.id.spinerInterest);
+
+
+        /*
+         Spinner spHobbies = findViewById(R.id.spinerInterest);
         ArrayAdapter<String> adapterArrayHobbies = new ArrayAdapter<>(this,  R.layout.spinner_item, hobbies);
         adapterArrayHobbies.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spHobbies.setPrompt("Hobbies");
         spHobbies.setAdapter(adapterArrayHobbies);
-        loadImgOnSpinner();
 
+
+         */
+        loadImgOnSpinner();
 
     }
     private void loadImgOnSpinner() {
