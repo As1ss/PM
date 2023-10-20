f9ea665ff87e32cef29504a5ba8dc6c66f706934
As1ss <alexis.lopbri@educa.jcyl.es>
2023-10-20 16:21:43 +0200

    Practica 3 mainActivity añadida

--- Practica3/app/src/main/java/al/if05/practica3/MainActivity.java
+++ Practica3/app/src/main/java/al/if05/practica3/MainActivity.java
@@ -3,12 +3,57 @@ package al.if05.practica3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    static int contador;
+    static int numMax;
+    static int numMin;
+    static String contadorString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText valorMax = findViewById(R.id.etMaxValor);
+
+
+
+
+        numMax = Integer.parseInt(String.valueOf(valorMax.getText()));
+
+
+
+        contador = 0;
+        contadorString = String.valueOf(contador);
+
+
+        Button buttonPlus = findViewById(R.id.btnSuma);
+        Button buttonMinus = findViewById(R.id.btnRestar);
+        TextView tvContador = findViewById(R.id.tvContador);
+
+
+
+        buttonPlus.setOnClickListener(view -> {
+
+
+            if(numMax>contador){
+                contador++;
+                contadorString = String.valueOf(contador); // Actualiza contadorString
+                tvContador.setText(contadorString);
+            }
+
+
+
+                Toast.makeText(this,String.valueOf(numMax),Toast.LENGTH_SHORT).show();
+        });
+
+
+
     }
 }
