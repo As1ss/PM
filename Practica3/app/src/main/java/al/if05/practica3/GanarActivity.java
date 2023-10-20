dfc2990ec9f67f42a0db263f8da2af1ad55bb141
As1ss <alexis.lopbri@educa.jcyl.es>
2023-10-20 21:46:59 +0200

    Practica 3 realizada y entregada

---
+++ Practica3/app/src/main/java/al/if05/practica3/GanarActivity.java
@@ -0,0 +1,14 @@
+package al.if05.practica3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GanarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ganar);
+    }
+}
