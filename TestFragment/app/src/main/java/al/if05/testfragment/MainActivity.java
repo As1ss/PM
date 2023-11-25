8da1b5bc672117e97807315569eda7edbf2209ed
As1ss <alexis.lopbri@educa.jcyl.es>
2023-11-25 14:21:26 +0100

    Creacion de un Fragment mediante codigo

--- TestFragment/app/src/main/java/al/if05/testfragment/MainActivity.java
+++ TestFragment/app/src/main/java/al/if05/testfragment/MainActivity.java
@@ -3,6 +3,9 @@ package al.if05.testfragment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tvHelloWOrld = findViewById(R.id.textView2);
+        Button button = findViewById(R.id.button);
+        String texto = tvHelloWOrld.getText().toString();
+        Bundle bundle = new Bundle();
+        bundle.putString("textoHolaMundo",texto);
+
+        button.setOnClickListener(view -> {
+            getSupportFragmentManager().beginTransaction().setReorderingAllowed(true).
+                    add(R.id.fragmentContainerView2, Fragmento1.class,bundle).commit();
+            Toast.makeText(this,"HOla", Toast.LENGTH_SHORT).show();
+        });
+
+
+
 
     }
 }
